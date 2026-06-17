8998426493065b3fcf06912e74c45be86ae291b4
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-06-17 10:30:34 +0300

    add: struct Point, Game, constructor newGame

--- main.go
+++ main.go
@@ -1,4 +1,37 @@
 package main
 
+type Point struct {
+	x int
+	y int
+}
+
+type Game struct {
+	snake    []Point
+	food     Point
+	malware  []Point
+	dir      Point
+	score    int
+	level    int
+	gameOver bool
+	width    int
+	height   int
+	quit     chan struct{}
+}
+
+func NewGame() *Game {
+	return &Game{
+		snake:    make([]Point, 0),
+		food:     Point{x: 4, y: 4},
+		malware:  make([]Point, 0),
+		dir:      Point{x: 1, y: 2},
+		score:    0,
+		level:    1,
+		gameOver: false,
+		height:   500,
+		width:    500,
+		quit:     make(chan struct{}),
+	}
+}
+
 func main() {
 }
